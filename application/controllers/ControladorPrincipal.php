a3b5162c43e6d0ba54f1d1369591325c1ce96057
eco4109 <37058383+eco4109@users.noreply.github.com>
2018-05-14 01:37:25 -0500

    Ultima Version Diego

--- application/controllers/ControladorPrincipal.php
+++ application/controllers/ControladorPrincipal.php
@@ -378,7 +378,54 @@ class ControladorPrincipal extends CI_Controller { //Definición principal
 	}
 
 	public function fRepoCalif(){ //Funcion para reporte de CALIFICACIONES (EL QUE GENERA EL PROFE)
+		$grupo = $this->input->post('grupo2');
+		$_SESSION['group2'] = $grupo;
+		$this->period = $this->input->post('period');
+		$_SESSION['periodoo'] = $this->period;
+		$tExa = $this->input->post('tExa');
 
+		$this->materia = $this->ModelosP->ObtenMateria($grupo);
+		$this->infoRepo = $this->ModelosP->ObtenCalifAlumnos($grupo, $this->period);
+		//var_dump($this->materia, $this->infoRepo);
+		//die();
+		$this->load->view('VRepoCalif', $this->materia, $this->infoRepo, $this->period);
+	}
+
+	public function fPDFCalifAlumnos(){ //Hace el PDF de lo de arriba
+		$this->materia = $this->ModelosP->ObtenMateria($_SESSION['group2']);
+		$this->infoRepo = $this->ModelosP->ObtenCalifAlumnos($_SESSION['group2'], $_SESSION['periodoo']);
+
+		//Generar el PDF 
+		$nombre = "Calificaciones del Grupo: ".$_SESSION['group2'].", Materia: ".$this->materia['nom_materia'].", Periodo: ".$_SESSION['periodoo'];
+
+		$pdf = new PDF('P', 'cm', 'a4');
+		$pdf->AddPage();
+		
+		$pdf->SetFont('Times','BU',18);
+		$pdf->Cell(19,1,$nombre,0,0,'C');
+		$pdf->Ln(1);
+		$pdf->Ln(1);
+		$pdf->SetFont('Times','B',14);
+		$pdf->SetDrawColor(0,80,180);
+		$pdf->SetFillColor(430,430,10);
+		$pdf->SetLineWidth(0.08);
+		$pdf->Cell(3, 1, "Id Alumno", 1, 0, 'C');
+		$pdf->Cell(5, 1, "Nombre Alumno", 1, 0, 'C');
+		$pdf->Cell(4, 1, "Tipo Examen", 1, 0, 'C');
+		$pdf->Cell(3, 1, "Calificacion", 1, 0, 'C');
+
+		$pdf->Ln();
+		$pdf->SetFont('Times','',12);
+
+		for ($i=0; $i < count($this->infoRepo); $i++) {
+			$pdf->Cell(3, 1, $this->infoRepo[$i]["id_alumno"], 1, 0, 'C');
+			$pdf->Cell(5, 1, $this->infoRepo[$i]["nom_alumno"], 1, 0, 'C');
+			$pdf->Cell(4, 1, $this->infoRepo[$i]["tipo_examen"], 1, 0, 'C');
+			$pdf->Cell(3, 1, $this->infoRepo[$i]["calificacion"], 1, 0, 'C');
+
+		}
+
+		$pdf->Output();
 	}
 
 	public function cargaVInscrGrupo(){ //Funcion para hacer una inscripción a un grupo
--- application/views/Vprofesor.php
+++ application/views/Vprofesor.php
@@ -25,13 +25,37 @@
 			<option selected >Grupo</option>
 			<?php
 				for ($i=0; $i < count($this->grupos); $i++) {?>
-					<option  class="form-control"><?php echo $this->grupos[$i]['id_grupo'] ;?></option>
+					<option><?php echo $this->grupos[$i]['id_grupo'] ;?></option>
 			<?php } ?>
 		</select>
 	</form>
 
 	<form action="fRepoCalif" method="post">
 		<input type="submit" value="Reporte de Calificaciones" style="margin-top: 10px; width: 175px;" >
+		<select name="grupo2"  id="grupo2" > 
+			<option selected >Grupo</option>
+			<?php
+				for ($i=0; $i < count($this->grupos); $i++) {?>
+					<option><?php echo $this->grupos[$i]['id_grupo'] ;?></option>
+			<?php } ?>
+		</select>
+
+		<select name="period"  id="period" > 
+			<option selected >Periodo</option>
+			<option value="2018A" >2018A</option>
+			<option value="2017B" >2017B</option>
+			<option value="2017A" >2017A</option>
+			<option value="2016B" >2016B</option>
+			<option value="2016A" >2016A</option>
+		</select>
+
+		<select name="tExa"  id="tExa" > 
+			<option selected >T. Examen</option>
+			<option value="1">Ordinario</option>
+			<option value="2" >Extraordinario</option>
+			<option value="3" >Titulo</option>
+			<option value="4" >Especial</option>
+		</select>
 	</form>
 
 	<form action="fsalir" method="post">
